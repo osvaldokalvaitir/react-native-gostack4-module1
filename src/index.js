e0a894f6b503c8097da22faef7ee5873e0b40b4a
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2019-01-27 13:41:43 -0200

    React DevTools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import './config/ReactotronConfig';
-
 import React, { Component } from 'react';
-import {
-  Text, StyleSheet, View, Button,
-} from 'react-native';
+
+import { StyleSheet, View } from 'react-native';
+
+import './config/ReactotronConfig';
+import './config/DevToolsConfig';
 
 import Todo from '~/components/Todo';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default class App extends Component {
   state = {
@@ -13,46 +24,12 @@ export default class App extends Component {
     todos: [{ id: 0, text: 'Fazer café' }, { id: 1, text: 'Estudar o Módulo 1' }],
   };
 
-  addTodo = () => {
-    this.setState({
-      todos: [...this.state.todos, { id: Math.random(), text: 'Novo todo' }],
-    });
-  };
-
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box}>
-          <Text style={styles.boxText}>Oi</Text>
-        </View>
+        <Todo title="Fazer café" />
+        <Todo title="Estudar o Módulo 1" />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    alignContent: 'center',
-  },
-
-  box: {
-    width: 80,
-    height: 80,
-    backgroundColor: '#F00',
-    margin: 10,
-    transform: [{ rotateZ: '20deg' }],
-  },
-
-  boxText: {
-    color: '#FFF',
-  },
-});
